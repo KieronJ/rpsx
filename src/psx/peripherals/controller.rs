e12ccbfb0cbf9149a04caba4f890ba790abd66ce
Kieron Josephs <kieron.josephs00@gmail.com>
2022-02-26 16:13:52 +0000

    controller: Add analogue pad state

--- src/psx/peripherals/controller.rs
+++ src/psx/peripherals/controller.rs
@@ -2,6 +2,8 @@ pub struct Controller {
     state: usize,
 
     pub button_select: bool,
+    pub button_l3: bool,
+    pub button_r3: bool,
     pub button_start: bool,
     pub button_dpad_up: bool,
     pub button_dpad_right: bool,
@@ -15,6 +17,11 @@ pub struct Controller {
     pub button_circle: bool,
     pub button_cross: bool,
     pub button_square: bool,
+
+    pub axis_lx: u8,
+    pub axis_ly: u8,
+    pub axis_rx: u8,
+    pub axis_ry: u8,
 }
 
 impl Controller {
@@ -23,6 +30,8 @@ impl Controller {
             state: 0,
 
             button_select: false,
+            button_l3: false,
+            button_r3: false,
             button_start: false,
             button_dpad_up: false,
             button_dpad_right: false,
@@ -36,6 +45,11 @@ impl Controller {
             button_circle: false,
             button_cross: false,
             button_square: false,
+
+            axis_lx: 0,
+            axis_ly: 0,
+            axis_rx: 0,
+            axis_ry: 0,
         }
     }
 
@@ -47,7 +61,7 @@ impl Controller {
             1 => {
                 if command == 0x42 {
                     self.state = 2;
-                    reply = 0x41;
+                    reply = 0x73;
                 } else {
                     self.state = 0;
                 }
@@ -62,6 +76,22 @@ impl Controller {
             },
             4 => {
                 reply = self.get_switch_state_hi();
+                self.state = 5;
+            },
+            5 => {
+                reply = self.axis_rx;
+                self.state = 6;
+            },
+            6 => {
+                reply = self.axis_ry;
+                self.state = 7;
+            },
+            7 => {
+                reply = self.axis_lx;
+                self.state = 8;
+            },
+            8 => {
+                reply = self.axis_ly;
                 self.state = 0;
             },
             _ => panic!("[CONTROLLER] [ERROR] Unknown state: {}", self.state),
@@ -101,6 +131,8 @@ impl Controller {
         value |= (self.button_dpad_right as u8) << 5;
         value |= (self.button_dpad_up as u8) << 4;
         value |= (self.button_start as u8) << 3;
+        value |= (self.button_r3 as u8) << 2;
+        value |= (self.button_l3 as u8) << 1;
         value |= self.button_select as u8;
 
         !value
